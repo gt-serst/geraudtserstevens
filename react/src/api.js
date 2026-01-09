5dbafb6361b1b46d6c2312b4eb6acd6aba63d0ba
Géraud t'Serstevens <geraudts@gmail.com>
2026-01-09 09:28:23 +0100

    centralize fetch requests

---
+++ react/src/api.js
@@ -0,0 +1,19 @@
+const API_URL = "http://localhost:8000/api";
+
+async function fetchRequest(endpoint, data) {
+	try {
+		const response = await fetch(API_URL + endpoint, {
+			method: "POST",
+			body: JSON.stringify(data),
+			headers: {"Content-type": "application/json; charset=UTF-8"}
+		});
+		const result = await response.json();
+
+		return { response, result };
+	} catch (error) {
+		return {
+			response: null,
+			result: { error: error.message }
+		};
+	}
+}
--- react/src/components/Register.js
+++ react/src/components/Register.js
@@ -2,34 +2,23 @@ import '../styles/Register.css'
 import { useForm } from 'react-hook-form';
 import { useState } from 'react'
 import { useNavigate } from "react-router-dom";
+import fetchRequest from "../api"
 
 function Register() {
 	const navigate = useNavigate();
 	const [serverErrors, setServerErrors] = useState(null);
 	const { register, handleSubmit, formState: { errors } } = useForm();
-	const url = "http://localhost:8000/api/auth/register/";
 
-	function onSubmit(data) {
-		const registerUser = async () => {
-			try {
-				const response = await fetch(url, {
-					method: "POST",
-					body: JSON.stringify(data),
-					headers: {"Content-type": "application/json; charset=UTF-8"}
-				});
-				const result = await response.json();
-				if (!response.ok) {
-					console.log(result)
-					setServerErrors(result)
-					return;
-				}
-				console.log(result);
-				navigate("/profile");
-			} catch (error) {
-				console.log(error.message);
-			}
-		};
-		registerUser();
+	async function onSubmit(data) {
+		const endpoint = "/auth/register/";
+
+		const { response, result } = await fetchRequest(endpoint, data);
+
+		if (!response || !response.ok) {
+			setServerErrors(result);
+			return;
+		}
+		navigate("/profile");
 	}
 
 	return (
